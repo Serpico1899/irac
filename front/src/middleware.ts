ef0b53c53b72dc77f5b40d9dabe9571e50da4ebb
serpico1899 <billywilder22@gmail.com>
2025-08-13 23:42:28 +0330

    Clean up i18n route matcher patterns

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -2,18 +2,11 @@ import createMiddleware from "next-intl/middleware";
 import { locales, defaultLocale } from "./config/i18n.config";
 
 export default createMiddleware({
-  // A list of all locales that are supported
   locales,
-
-  // Used when no locale matches
   defaultLocale,
-
-  // The `localePrefix` option is set to `always` to ensure
-  // a locale is always part of the URL path.
   localePrefix: "always",
 });
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
