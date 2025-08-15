49b9988205a62aa1cb983bc6a5411ae16b09c35e
serpico1899 <billywilder22@gmail.com>
2025-08-15 23:58:53 +0330

    Remove unused Badge component

--- front/src/components/molecules/Badge.tsx
+++
@@ -1,8 +0,0 @@
-
-export const Badge = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <span className="inline-block bg-indigo-100 text-indigo-800 text-xs px-2 py-1 rounded-full font-medium">
-      {children}
-    </span>
-  );
-};
