3aa3a0768eb728109b58b6d66be59fdd9522e086
serpico1899 <billywilder22@gmail.com>
2025-08-26 01:28:56 +0330

    Fix formatting in user edit page

--- front/src/app/[locale]/admin/users/edit/pure/[id]/page.tsx
+++ front/src/app/[locale]/admin/users/edit/pure/[id]/page.tsx
@@ -9,15 +9,20 @@ const FormComponent = async ({
 }) => {
   const id = (await params).id;
 
-  const foundedUser = await getUser(id)
-  const lesanUrl = process.env.LESAN_URL ? process.env.LESAN_URL : "http://localhost:1382"
+  const foundedUser = await getUser(id);
+  const lesanUrl = process.env.LESAN_URL
+    ? process.env.LESAN_URL
+    : "http://localhost:1382";
 
   return (
     <>
       {foundedUser.success ? (
         <div className="p-6 min-h-full">
           <div className="flex items-center justify-between mb-6">
-            <h1 className="text-2xl font-bold text-gray-700">ویرایش «{foundedUser.body.first_name} {foundedUser.body.last_name}»</h1>
+            <h1 className="text-2xl font-bold text-gray-700">
+              ویرایش «{foundedUser.body.first_name} {foundedUser.body.last_name}
+              »
+            </h1>
             <Link
               href="/admin/users"
               className="px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 transition-colors"
@@ -30,14 +35,12 @@ const FormComponent = async ({
       ) : (
         <div>
           مشکلی در یافتن کاربر وجود دارد - {foundedUser.body.message}
-
           <Link
             href="/admin/users"
             className="px-6 py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
           >
             بازگشت
           </Link>
-
         </div>
       )}
     </>
