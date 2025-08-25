1455f4c2530b72605df6b9e300fff04c14a53ccd
serpico1899 <billywilder22@gmail.com>
2025-08-26 01:26:57 +0330

    Add internationalization to login page titles

--- front/src/app/[locale]/login/page.tsx
+++ front/src/app/[locale]/login/page.tsx
@@ -4,11 +4,13 @@ import LoginStepTwo from "@/components/organisms/login/LoginStepTwo";
 import { useAutoReturnTimer } from "@/hooks/useAutoReturnTimer";
 import { useState } from "react";
 import { useAuth } from "@/context/AuthContext";
+import { useTranslations } from "next-intl";
 
 const Login = ({}) => {
   const [phone, setPhone] = useState("");
   const [step, setStep] = useState(1);
   const { login } = useAuth();
+  const t = useTranslations("Login");
 
   const { clearAutoReturnTimer, remainingTime } = useAutoReturnTimer(
     step,
@@ -34,9 +36,7 @@ const Login = ({}) => {
       <div className="h-[calc(100vh-9rem)] bg-gray-100 flex items-center justify-center">
         <div className="bg-white p-6 rounded-lg shadow-lg w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4">
           <h1 className="text-xl sm:text-2xl font-bold mb-4 text-center text-gray-700">
-            {step === 1
-              ? "کد ملی خود را وارد کنید"
-              : "کد ارسال شده را وارد کنید"}
+            {step === 1 ? t("stepOneTitle") : t("stepTwoTitle")}
           </h1>
           {step === 1 ? (
             <LoginStepOne setStep={setStep} setPhone={setPhone} phone={phone} />
