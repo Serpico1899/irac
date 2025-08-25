748638234f842bb76518cad1abc920089794a227
serpico1899 <billywilder22@gmail.com>
2025-08-26 01:23:48 +0330

    Replace hardcoded select placeholder with i18n translation

--- front/src/components/atoms/Select.tsx
+++ front/src/components/atoms/Select.tsx
@@ -9,8 +9,13 @@ import {
   Path,
 } from "react-hook-form";
 import { ReactSelectOption } from "@/types/option";
+import { useTranslations } from "next-intl";
 
-interface SelectBoxProps<Option, Group extends GroupBase<Option>, T extends FieldValues = FieldValues> {
+interface SelectBoxProps<
+  Option,
+  Group extends GroupBase<Option>,
+  T extends FieldValues = FieldValues,
+> {
   name: FieldPath<T>;
   label: string;
   setValue: UseFormSetValue<T>;
@@ -18,46 +23,52 @@ interface SelectBoxProps<Option, Group extends GroupBase<Option>, T extends Fiel
   placeholder?: string;
   labelAsValue?: boolean;
   className?: string;
-  errMsg?: string
+  errMsg?: string;
   defaultValue?: PropsValue<ReactSelectOption>;
   defaultOptions?: OptionsOrGroups<Option, Group> | boolean;
 }
 
-const SelectBox = <Option, Group extends GroupBase<Option>, T extends FieldValues = FieldValues>({
+const SelectBox = <
+  Option,
+  Group extends GroupBase<Option>,
+  T extends FieldValues = FieldValues,
+>({
   name,
   setValue,
   label,
   options,
   errMsg,
-  placeholder = "انتخاب کنید",
+  placeholder,
   className = "",
   labelAsValue,
-  defaultValue
+  defaultValue,
 }: SelectBoxProps<Option, Group, T>) => {
+  const t = useTranslations("Common");
 
   return (
     <div className={`w-1/2 p-4 flex flex-col gap-1 ${className}`}>
       {label && (
-        <label
-          htmlFor={name}
-          className="text-sm font-medium text-gray-700"
-        >
+        <label htmlFor={name} className="text-sm font-medium text-gray-700">
           {label}
         </label>
       )}
       <Select
         id={name}
         options={options}
-        onChange={(newVal) => setValue(name, (labelAsValue ? newVal!.label : newVal!.value) as unknown as PathValue<T, Path<T>>)}
-        placeholder={placeholder}
+        onChange={(newVal) =>
+          setValue(
+            name,
+            (labelAsValue
+              ? newVal!.label
+              : newVal!.value) as unknown as PathValue<T, Path<T>>,
+          )
+        }
+        placeholder={placeholder || t("selectPlaceholder")}
         classNamePrefix="react-select"
-        className={`text-sm ${errMsg ? "border-red-500" : "border-gray-300"
-          }`}
+        className={`text-sm ${errMsg ? "border-red-500" : "border-gray-300"}`}
         defaultValue={defaultValue}
       />
-      {errMsg && (
-        <span className="text-red-500 text-xs">{errMsg}</span>
-      )}
+      {errMsg && <span className="text-red-500 text-xs">{errMsg}</span>}
     </div>
   );
 };
