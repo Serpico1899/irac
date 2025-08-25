41b671b658dfead4f08787129a012fc57f3cb54d
serpico1899 <billywilder22@gmail.com>
2025-08-26 01:29:15 +0330

    Add translations to create user page

--- front/src/app/[locale]/admin/users/createUser/page.tsx
+++ front/src/app/[locale]/admin/users/createUser/page.tsx
@@ -1,19 +1,21 @@
 import { FormCreateUser } from "@/components/template/FormCreateUser";
 import { cookies } from "next/headers";
 import Link from "next/link";
+import { getTranslations } from "next-intl/server";
 
 const Page = async () => {
   const token = (await cookies()).get("token");
+  const t = await getTranslations("Admin");
 
   return (
     <div className="p-6 min-h-full">
       <div className="flex items-center justify-between mb-6">
-        <h1 className="text-2xl font-bold text-gray-700">ایجاد کاربر جدید</h1>
+        <h1 className="text-2xl font-bold text-gray-700">{t("createUser")}</h1>
         <Link
           href="/admin/users"
           className="px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 transition-colors"
         >
-          بازگشت به لیست
+          {t("backToList")}
         </Link>
       </div>
       <FormCreateUser token={token?.value} />
