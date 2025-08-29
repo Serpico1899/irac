ae65566c3e697e5e6326e109a9f6040f0f4b8c09
serpico1899 <billywilder22@gmail.com>
2025-08-29 08:05:55 +0330

    Rename color tokens and expand text color palette

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -19,15 +19,29 @@ const config: Config = {
           dark: "#0f7882",
         },
         accent: {
-          DEFAULT: "#cea87a", // Bronze/Gold accent
+          DEFAULT: "#cea87a", // Bronze accent
+          primary: "#facc15", // Gold accent
         },
         background: {
-          DEFAULT: "#FFFFFF", // White
-          light: "#F5F7FA", // Light Gray-Blue
+          DEFAULT: "#FFFFFF", // background
+          // light: "#F5F7FA", //
+          primary: "#F5F7FA",
+          // darker: "#F3F4F6", // Darker Gray-Blue
+          secondary: "#F3F4F6", // gray-100
+          darkest: "#E5E7EB", // gray-200
         },
         text: {
-          DEFAULT: "#333333", // Dark Gray for headings
-          light: "#777777", // Medium Gray for sub-text
+          DEFAULT: "#000000", // black
+          // light: "#3D3D3D",
+          primary: "#3D3D3D",
+          // lighter: "#4B5563", // gray-600
+          secondary: "#4B5563",
+          // lightest: "#6B7280", // gray-500
+          light: "#6B7280",
+          // lightestest: "#9CA3AF", // gray-400
+          lighter: "#9CA3AF",
+          // lightestestest: "#D1D5DB", // gray-300
+          lightest: "#D1D5DB",
         },
       },
     },
