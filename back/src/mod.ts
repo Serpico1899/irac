011c32976c03af4c3b6bf64dc7ce2b04798fc5e6
serpico1899 <billywilder22@gmail.com>
2025-08-30 11:24:57 +0330

    Add course setup to functions initialization

--- back/src/mod.ts
+++ back/src/mod.ts
@@ -2,10 +2,12 @@ import { fileSetup } from "./file/mod.ts";
 import { userSetup } from "./user/mod.ts";
 import { tagSetup } from "./tag/mod.ts";
 import { categorySetup } from "./category/mod.ts";
+import { courseSetup } from "./course/mod.ts";
 
 export const functionsSetup = () => {
-	fileSetup();
-	userSetup();
-	tagSetup();
-	categorySetup();
+  fileSetup();
+  userSetup();
+  tagSetup();
+  categorySetup();
+  courseSetup();
 };
