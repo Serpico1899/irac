bb1fa592817e175a570566b1f5d46bcc57c5b906
serpico1899 <billywilder22@gmail.com>
2025-08-30 13:31:45 +0330

    Add validator for home page data

---
+++ back/src/page/getHomePageData/getHomePageData.val.ts
@@ -0,0 +1,35 @@
+import { object, optional } from "../../../../deps.ts";
+
+export const getHomePageDataValidator = () => {
+  return object({
+    set: optional(object({})),
+    get: optional(object({
+      courses: optional(object({
+        title: optional(object({ en: optional(object({})), fa: optional(object({})) })),
+        description: optional(object({ en: optional(object({})), fa: optional(object({})) })),
+        instructorName: optional(object({})),
+        price: optional(object({})),
+        duration: optional(object({})),
+        imageUrl: optional(object({})),
+        createdAt: optional(object({})),
+        isFeatured: optional(object({})),
+      })),
+      articles: optional(object({
+        title: optional(object({ en: optional(object({})), fa: optional(object({})) })),
+        description: optional(object({ en: optional(object({})), fa: optional(object({})) })),
+        author: optional(object({})),
+        imageUrl: optional(object({})),
+        createdAt: optional(object({})),
+        slug: optional(object({})),
+      })),
+      products: optional(object({
+        title: optional(object({ en: optional(object({})), fa: optional(object({})) })),
+        description: optional(object({ en: optional(object({})), fa: optional(object({})) })),
+        price: optional(object({})),
+        imageUrl: optional(object({})),
+        createdAt: optional(object({})),
+        category: optional(object({})),
+      })),
+    })),
+  });
+};
