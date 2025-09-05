8e2a975cfc1fd3256131024e39606549439f58fa
serpico1899 <billywilder22@gmail.com>
2025-09-05 22:41:17 +0330

    Add course, article and wallet module setups

--- back/src/mod.ts
+++ back/src/mod.ts
@@ -2,10 +2,15 @@ import { fileSetup } from "./file/mod.ts";
 import { userSetup } from "./user/mod.ts";
 import { tagSetup } from "./tag/mod.ts";
 import { categorySetup } from "./category/mod.ts";
+import { courseSetup } from "./course/mod.ts";
+import { articleSetup } from "./article/mod.ts";
+import "./wallet/mod.ts";
 
 export const functionsSetup = () => {
-	fileSetup();
-	userSetup();
-	tagSetup();
-	categorySetup();
+  fileSetup();
+  userSetup();
+  tagSetup();
+  categorySetup();
+  courseSetup();
+  articleSetup();
 };
