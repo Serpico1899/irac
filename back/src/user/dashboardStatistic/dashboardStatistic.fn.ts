10bb088d03f884df7b0ce3c59e22352ec5515691
serpico1899 <billywilder22@gmail.com>
2025-09-05 22:41:10 +0330

    Update dashboard statistics to show content counts

--- back/src/user/dashboardStatistic/dashboardStatistic.fn.ts
+++ back/src/user/dashboardStatistic/dashboardStatistic.fn.ts
@@ -1,30 +1,27 @@
 import type { ActFn } from "@deps";
 import {
-	category,
-	city,
-	city_zone,
-	location,
-	province,
-	tag,
-	user,
+  article,
+  category,
+  course,
+  file,
+  tag,
+  user,
 } from "../../../mod.ts";
 
 export const dashboardStatisticFn: ActFn = async () => {
-	const categories = await category.countDocument({});
-	const cities = await city.countDocument({});
-	const city_zones = await city_zone.countDocument({});
-	const locations = await location.countDocument({}); // Added await
-	const provinces = await province.countDocument({});
-	const tags = await tag.countDocument({});
-	const users = await user.countDocument({});
+  const articles = await article.countDocument({});
+  const categories = await category.countDocument({});
+  const courses = await course.countDocument({});
+  const files = await file.countDocument({});
+  const tags = await tag.countDocument({});
+  const users = await user.countDocument({});
 
-	return {
-		categories,
-		cities,
-		city_zones,
-		locations,
-		provinces,
-		tags,
-		users,
-	};
+  return {
+    articles,
+    categories,
+    courses,
+    files,
+    tags,
+    users,
+  };
 };
