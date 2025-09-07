52a9e0712756a6b951defdfb1b17094f256dc3ce
serpico1899 <billywilder22@gmail.com>
2025-09-07 12:31:30 +0330

    Fix indentation to use spaces instead of tabs

--- back/models/utils/sharedRelaions.ts
+++ back/models/utils/sharedRelaions.ts
@@ -2,17 +2,17 @@ import { RelationDataType, string } from "@deps";
 import { createUpdateAt } from "../../utils/createUpdateAt.ts";
 
 export const shared_relation_pure = {
-	name: string(),
-	description: string(),
+  name: string(),
+  description: string(),
 
-	...createUpdateAt,
+  ...createUpdateAt,
 };
 
 export const createSharedRelations = () => ({
-	registrer: {
-		schemaName: "user",
-		type: "single" as RelationDataType,
-		optional: true,
-		relatedRelations: {},
-	},
+  registrer: {
+    schemaName: "user",
+    type: "single" as RelationDataType,
+    optional: true,
+    relatedRelations: {},
+  },
 });
