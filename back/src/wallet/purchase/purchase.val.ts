38b5b31076e28af60653eb0257c8dc7241122ede
serpico1899 <billywilder22@gmail.com>
2025-09-07 12:33:40 +0330

    Add validator schema for purchase transactions

---
+++ back/src/wallet/purchase/purchase.val.ts
@@ -0,0 +1,14 @@
+import { object, objectIdValidation, number, string, optional } from "@deps";
+import { selectStruct } from "../../../mod.ts";
+
+export const purchaseValidator = () => {
+  return object({
+    set: object({
+      user_id: objectIdValidation,
+      amount: number(),
+      order_id: string(),
+      description: optional(string()),
+    }),
+    get: selectStruct("wallet_transaction", 2),
+  });
+};
