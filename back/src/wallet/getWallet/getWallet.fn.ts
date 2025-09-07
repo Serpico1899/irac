6843de556d82b3988762ffd6a9370bd4f96ae069
serpico1899 <billywilder22@gmail.com>
2025-09-07 12:34:35 +0330

    Add wallet retrieval endpoint

---
+++ back/src/wallet/getWallet/getWallet.fn.ts
@@ -0,0 +1,23 @@
+import { type ActFn, ObjectId } from "@deps";
+import { wallet } from "../../../mod.ts";
+import { WalletService } from "../service.ts";
+
+export const getWalletFn: ActFn = async (body) => {
+  const {
+    set: { user_id },
+    get,
+  } = body.details;
+
+  // Get or create wallet for the user
+  const userWallet = await WalletService.getUserWallet(user_id);
+
+  // Use aggregation to get wallet with proper projection
+  const foundedWallet = await wallet
+    .aggregation({
+      pipeline: [{ $match: { _id: new ObjectId(userWallet._id) } }],
+      projection: get,
+    })
+    .toArray();
+
+  return foundedWallet[0];
+};
