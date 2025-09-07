7d0ed0df292966a5d546de6c68dc672057b66525
serpico1899 <billywilder22@gmail.com>
2025-09-07 12:34:46 +0330

    Add transaction filtering validation schema

---
+++ back/src/wallet/getTransactions/getTransactions.val.ts
@@ -0,0 +1,15 @@
+import { object, objectIdValidation, number, string, optional } from "@deps";
+import { selectStruct } from "../../../mod.ts";
+
+export const getTransactionsValidator = () => {
+  return object({
+    set: object({
+      user_id: objectIdValidation,
+      page: optional(number()),
+      limit: optional(number()),
+      type: optional(string()),
+      status: optional(string()),
+    }),
+    get: selectStruct("wallet_transaction", 2),
+  });
+};
