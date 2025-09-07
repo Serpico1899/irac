d925ebbaa98c78848d25c794daa9b4c88921f95a
serpico1899 <billywilder22@gmail.com>
2025-09-07 12:39:51 +0330

    Add payment creation endpoint

---
+++ back/src/payment/createPayment/mod.ts
@@ -0,0 +1,11 @@
+import { setAct } from "../../../mod.ts";
+import { createPaymentValidator } from "./createPayment.val.ts";
+import { createPaymentFn } from "./createPayment.fn.ts";
+
+export const createPaymentSetup = () =>
+  setAct({
+    schema: "payment",
+    actName: "createPayment",
+    validator: createPaymentValidator(),
+    fn: createPaymentFn,
+  });
