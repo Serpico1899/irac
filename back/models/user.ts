6e7bb7cc1064a51f2570fff84c1158ff6c8e260b
serpico1899 <billywilder22@gmail.com>
2025-09-15 13:03:34 +0330

    Fix indentation in user model

--- back/models/user.ts
+++ back/models/user.ts
@@ -1,17 +1,17 @@
 import { coreApp } from "../mod.ts";
 import {
-	boolean,
-	coerce,
-	date,
-	defaulted,
-	enums,
-	number,
-	optional,
-	pattern,
-	refine,
-	type RelationDataType,
-	string,
-	union,
+  boolean,
+  coerce,
+  date,
+  defaulted,
+  enums,
+  number,
+  optional,
+  pattern,
+  refine,
+  type RelationDataType,
+  string,
+  union,
 } from "@deps";
 import { createUpdateAt, isValidNationalNumber } from "@lib";
 
@@ -19,17 +19,17 @@ export const user_level_array = ["Ghost", "Manager", "Editor", "Ordinary"];
 export const user_level_emums = enums(user_level_array);
 
 export const mobile_pattern = pattern(
-	string(),
-	/(\+98|0|98|0098)?([ ]|-|[()]){0,2}9[0-9]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/,
+  string(),
+  /(\+98|0|98|0098)?([ ]|-|[()]){0,2}9[0-9]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/,
 );
 
 export const is_valid_national_number_struct = refine(
-	union([string(), number()]),
-	"national_number",
-	(value: string | number) => {
-		const normalized = String(value).trim();
-		return isValidNationalNumber(normalized);
-	},
+  union([string(), number()]),
+  "national_number",
+  (value: string | number) => {
+    const normalized = String(value).trim();
+    return isValidNationalNumber(normalized);
+  },
 );
 
 // export const is_valid_national_number_struct = define<string>(
@@ -46,42 +46,42 @@ export const is_valid_national_number_struct = refine(
 export const user_genders = enums(["Male", "Female"]);
 
 export const user_pure = {
-	first_name: string(),
-	last_name: string(),
-	father_name: string(),
-	mobile: mobile_pattern,
-	gender: user_genders,
-	birth_date: optional(coerce(date(), string(), (value) => new Date(value))),
-	summary: optional(string()),
+  first_name: string(),
+  last_name: string(),
+  father_name: string(),
+  mobile: mobile_pattern,
+  gender: user_genders,
+  birth_date: optional(coerce(date(), string(), (value) => new Date(value))),
+  summary: optional(string()),
 
-	// شماره ملی
-	national_number: is_valid_national_number_struct,
-	address: string(),
+  // شماره ملی
+  national_number: is_valid_national_number_struct,
+  address: string(),
 
-	level: user_level_emums,
-	is_verified: defaulted(boolean(), false),
-	...createUpdateAt,
+  level: user_level_emums,
+  is_verified: defaulted(boolean(), false),
+  ...createUpdateAt,
 };
 
 export const user_relations = {
-	avatar: {
-		schemaName: "file",
-		type: "single" as RelationDataType,
-		optional: true,
-		relatedRelations: {},
-	},
-	national_card: {
-		schemaName: "file",
-		type: "single" as RelationDataType,
-		optional: true,
-		relatedRelations: {},
-	},
+  avatar: {
+    schemaName: "file",
+    type: "single" as RelationDataType,
+    optional: true,
+    relatedRelations: {},
+  },
+  national_card: {
+    schemaName: "file",
+    type: "single" as RelationDataType,
+    optional: true,
+    relatedRelations: {},
+  },
 };
 
 export const users = () =>
-	coreApp.odm.newModel("user", user_pure, user_relations, {
-		createIndex: {
-			indexSpec: { "national_number": 1 },
-			options: { unique: true },
-		},
-	});
+  coreApp.odm.newModel("user", user_pure, user_relations, {
+    createIndex: {
+      indexSpec: { "national_number": 1 },
+      options: { unique: true },
+    },
+  });
