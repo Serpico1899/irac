32b5360642fcd0b5d0bdad4e2b0c8864f2d25cf5
serpico1899 <billywilder22@gmail.com>
2025-09-15 10:19:43 +0330

    Add generateReferralCode action setup

---
+++ back/src/referral/generateReferralCode/mod.ts
@@ -0,0 +1,19 @@
+import { coreApp } from "../../../mod.ts";
+import { generateReferralCodeValidator } from "./generateReferralCode.val.ts";
+import { generateReferralCodeFn } from "./generateReferralCode.fn.ts";
+import { grantAccess, setTokens, setUser } from "@lib";
+
+export const generateReferralCodeSetup = () =>
+  coreApp.acts.setAct({
+    schema: "referral",
+    actName: "generateReferralCode",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager", "Editor", "Ordinary"],
+      }),
+    ],
+    validator: generateReferralCodeValidator(),
+    fn: generateReferralCodeFn,
+  });
