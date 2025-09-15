bffa75888d09ea1fca55218e73e790e6ef60ee18
serpico1899 <billywilder22@gmail.com>
2025-09-15 10:16:23 +0330

    Add addPoints scoring transaction endpoint

---
+++ back/src/scoring/addPoints/mod.ts
@@ -0,0 +1,19 @@
+import { coreApp } from "../../../mod.ts";
+import { addPointsValidator } from "./addPoints.val.ts";
+import { addPointsFn } from "./addPoints.fn.ts";
+import { grantAccess, setTokens, setUser } from "@lib";
+
+export const addPointsSetup = () =>
+  coreApp.acts.setAct({
+    schema: "scoring_transaction",
+    actName: "addPoints",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager", "Editor", "Ordinary"],
+      }),
+    ],
+    validator: addPointsValidator(),
+    fn: addPointsFn,
+  });
