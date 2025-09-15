4e7ce75a3f1734289740665de82fad370dab141f
serpico1899 <billywilder22@gmail.com>
2025-09-15 10:23:05 +0330

    Add track downloads module

---
+++ back/src/download/trackDownloads/mod.ts
@@ -0,0 +1,13 @@
+import { setAct } from "../../../mod.ts";
+import { trackDownloadsValidator } from "./trackDownloads.val.ts";
+import { trackDownloadsFn } from "./trackDownloads.fn.ts";
+
+export const trackDownloadsSetup = () => {
+  setAct({
+    schema: {
+      details: trackDownloadsValidator.schema.details,
+    },
+    validator: trackDownloadsValidator,
+    fn: trackDownloadsFn,
+  });
+};
