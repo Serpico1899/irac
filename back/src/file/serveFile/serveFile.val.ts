f2f8d9125a798ef0983bfbb97834faf863661690
serpico1899 <billywilder22@gmail.com>
2025-09-15 10:21:40 +0330

    Add serveFile validator with file serving parameters

---
+++ back/src/file/serveFile/serveFile.val.ts
@@ -0,0 +1,13 @@
+import { object, string, boolean, optional } from "@deps";
+import { coreApp } from "../../../mod.ts";
+
+export const serveFileValidator = () => {
+  return object({
+    set: object({
+      fileId: string(),
+      download: optional(boolean()),
+      track: optional(boolean()),
+    }),
+    get: coreApp.schemas.selectStruct("file", 1),
+  });
+};
