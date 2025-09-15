58beda71016a49447cd6bc831f84c89172b895eb
serpico1899 <billywilder22@gmail.com>
2025-09-15 10:15:32 +0330

    Add getLeaderboard endpoint to user_level schema

---
+++ back/src/scoring/getLeaderboard/mod.ts
@@ -0,0 +1,19 @@
+import { coreApp } from "../../../mod.ts";
+import { getLeaderboardValidator } from "./getLeaderboard.val.ts";
+import { getLeaderboardFn } from "./getLeaderboard.fn.ts";
+import { grantAccess, setTokens, setUser } from "@lib";
+
+export const getLeaderboardSetup = () =>
+  coreApp.acts.setAct({
+    schema: "user_level",
+    actName: "getLeaderboard",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager", "Editor", "Ordinary"],
+      }),
+    ],
+    validator: getLeaderboardValidator(),
+    fn: getLeaderboardFn,
+  });
