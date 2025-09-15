932431046fb6d2f503e31bc305d7770d1c80de99
serpico1899 <billywilder22@gmail.com>
2025-09-15 13:03:20 +0330

    Add serve file setup to file module

--- back/src/file/mod.ts
+++ back/src/file/mod.ts
@@ -1,6 +1,7 @@
 import { getFilesSetup } from "./getFiles/mod.ts";
 import { uploadFileSetup } from "./uploadFile/mod.ts";
+import { serveFileSetup } from "./serveFile/mod.ts";
 
 export const fileSetup = () => {
-	getFilesSetup(), uploadFileSetup();
+  getFilesSetup(), uploadFileSetup(), serveFileSetup();
 };
