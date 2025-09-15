ee88fd7e5daf6be21a76d20b906f32fc0b6c04da
serpico1899 <billywilder22@gmail.com>
2025-09-15 10:22:02 +0330

    Add file serving action setup with validation

---
+++ back/src/file/serveFile/mod.ts
@@ -0,0 +1,17 @@
+import { coreApp } from "../../../mod.ts";
+import { setTokens, setUser } from "@lib";
+import { serveFileFn } from "./serveFile.fn.ts";
+import { serveFileValidator } from "./serveFile.val.ts";
+
+export const serveFileSetup = () =>
+  coreApp.acts.setAct({
+    schema: "file",
+    actName: "serveFile",
+    validationRunType: "get",
+    validator: serveFileValidator(),
+    preAct: [
+      setTokens,
+      setUser,
+    ],
+    fn: serveFileFn,
+  });
