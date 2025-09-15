727b9b08167088024a68e5d578d67604b35601de
serpico1899 <billywilder22@gmail.com>
2025-09-15 10:14:12 +0330

    Add daily login processing endpoint

---
+++ back/src/scoring/processDailyLogin/mod.ts
@@ -0,0 +1,19 @@
+import { coreApp } from "../../../mod.ts";
+import { processDailyLoginValidator } from "./processDailyLogin.val.ts";
+import { processDailyLoginFn } from "./processDailyLogin.fn.ts";
+import { grantAccess, setTokens, setUser } from "@lib";
+
+export const processDailyLoginSetup = () =>
+  coreApp.acts.setAct({
+    schema: "user_level",
+    actName: "processDailyLogin",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager", "Editor", "Ordinary"],
+      }),
+    ],
+    validator: processDailyLoginValidator(),
+    fn: processDailyLoginFn,
+  });
