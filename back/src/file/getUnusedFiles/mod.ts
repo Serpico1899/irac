59068c10fec0f4693cde1edf8e3f959f7f74998f
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:27:19 +0330

    Add endpoint to retrieve unused files for managers

---
+++ back/src/file/getUnusedFiles/mod.ts
@@ -0,0 +1,20 @@
+import { coreApp } from "../../../mod.ts";
+import { setTokens, setUser, grantAccess } from "@lib";
+import { getUnusedFilesFn } from "./getUnusedFiles.fn.ts";
+import { getUnusedFilesValidator } from "./getUnusedFiles.val.ts";
+
+export const getUnusedFilesSetup = () =>
+  coreApp.acts.setAct({
+    schema: "file",
+    actName: "getUnusedFiles",
+    validationRunType: "get",
+    validator: getUnusedFilesValidator(),
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager"],
+      }),
+    ],
+    fn: getUnusedFilesFn,
+  });
