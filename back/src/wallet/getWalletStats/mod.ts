0b99ed8c402ed35ea4f45044a64a4e362958f0c8
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:23:19 +0330

    Add wallet stats endpoint for managers

---
+++ back/src/wallet/getWalletStats/mod.ts
@@ -0,0 +1,20 @@
+import { coreApp } from "../../../mod.ts";
+import { getWalletStatsFn } from "./getWalletStats.fn.ts";
+import { getWalletStatsValidator } from "./getWalletStats.val.ts";
+import { grantAccess, setTokens, setUser } from "@lib";
+
+export const getWalletStatsSetup = () =>
+  coreApp.acts.setAct({
+    schema: "wallet",
+    actName: "getWalletStats",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager"],
+        features: ["wallet:admin"],
+      }),
+    ],
+    validator: getWalletStatsValidator(),
+    fn: getWalletStatsFn,
+  });
