a95294d2f6a8404dabcb72ed217f75741650c78c
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:23:58 +0330

    Add endpoint to get all wallets with admin access

---
+++ back/src/wallet/getAllWallets/mod.ts
@@ -0,0 +1,20 @@
+import { coreApp } from "../../../mod.ts";
+import { getAllWalletsFn } from "./getAllWallets.fn.ts";
+import { getAllWalletsValidator } from "./getAllWallets.val.ts";
+import { grantAccess, setTokens, setUser } from "@lib";
+
+export const getAllWalletsSetup = () =>
+  coreApp.acts.setAct({
+    schema: "wallet",
+    actName: "getAllWallets",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager"],
+        features: ["wallet:admin"],
+      }),
+    ],
+    validator: getAllWalletsValidator(),
+    fn: getAllWalletsFn,
+  });
