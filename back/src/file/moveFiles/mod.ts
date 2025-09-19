6e58141c3efdf53c04a4f9b13539a76b31a08ae1
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:27:13 +0330

    Add moveFiles action to file schema

---
+++ back/src/file/moveFiles/mod.ts
@@ -0,0 +1,20 @@
+import { coreApp } from "../../../mod.ts";
+import { setTokens, setUser, grantAccess } from "@lib";
+import { moveFilesFn } from "./moveFiles.fn.ts";
+import { moveFilesValidator } from "./moveFiles.val.ts";
+
+export const moveFilesSetup = () =>
+  coreApp.acts.setAct({
+    schema: "file",
+    actName: "moveFiles",
+    validationRunType: "update",
+    validator: moveFilesValidator(),
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager"],
+      }),
+    ],
+    fn: moveFilesFn,
+  });
