76ed1ccfc299ba0e352e1a4a5a189dc6f284f78f
serpico1899 <billywilder22@gmail.com>
2025-09-19 16:38:54 +0330

    Add enrollment update endpoint with auth checks

---
+++ back/src/enrollment/updateEnrollment/mod.ts
@@ -0,0 +1,25 @@
+import {
+  grantAccess,
+  setTokens,
+  setUser,
+} from "@lib";
+import { coreApp } from "../../../mod.ts";
+import { updateEnrollmentFn } from "./updateEnrollment.fn.ts";
+import { updateEnrollmentValidator } from "./updateEnrollment.val.ts";
+
+export const updateEnrollmentSetup = () =>
+  coreApp.acts.setAct({
+    schema: "enrollment",
+    actName: "updateEnrollment",
+    validationRunType: "update",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager", "Admin"],
+        users: "me",
+      }),
+    ],
+    validator: updateEnrollmentValidator(),
+    fn: updateEnrollmentFn,
+  });
