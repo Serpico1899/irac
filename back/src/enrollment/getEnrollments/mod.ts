10db255e93e1a7c8e60e9f6e0ea1402101b8734a
serpico1899 <billywilder22@gmail.com>
2025-09-19 16:39:07 +0330

    Add getEnrollments endpoint for enrollment module

---
+++ back/src/enrollment/getEnrollments/mod.ts
@@ -0,0 +1,25 @@
+import {
+  grantAccess,
+  setTokens,
+  setUser,
+} from "@lib";
+import { coreApp } from "../../../mod.ts";
+import { getEnrollmentsFn } from "./getEnrollments.fn.ts";
+import { getEnrollmentsValidator } from "./getEnrollments.val.ts";
+
+export const getEnrollmentsSetup = () =>
+  coreApp.acts.setAct({
+    schema: "enrollment",
+    actName: "getEnrollments",
+    validationRunType: "read",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager", "Admin"],
+        users: "me",
+      }),
+    ],
+    validator: getEnrollmentsValidator(),
+    fn: getEnrollmentsFn,
+  });
