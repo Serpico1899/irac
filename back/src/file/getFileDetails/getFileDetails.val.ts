52362ec983f2e42fa5ee4fced2ba9db6cfa69143
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:28:08 +0330

    Add file details validator with get/set params

---
+++ back/src/file/getFileDetails/getFileDetails.val.ts
@@ -0,0 +1,16 @@
+import { boolean, object, optional, string } from "@deps";
+import { coreApp } from "../../../mod.ts";
+
+export const getFileDetailsValidator = () => {
+  return object({
+    set: object({
+      _id: string(),
+      include_usage_stats: optional(boolean()), // Include file usage statistics
+      include_references: optional(boolean()), // Include references from other models
+      include_physical_info: optional(boolean()), // Include physical file system info
+      include_upload_history: optional(boolean()), // Include upload and modification history
+      include_related_files: optional(boolean()), // Include related/similar files
+    }),
+    get: coreApp.schemas.selectStruct("file", 2),
+  });
+};
