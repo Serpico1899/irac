4e4fab21177822f7446adebeabc525eaf7eb3584
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:28:22 +0330

    Add deleteFile endpoint in file module

---
+++ back/src/file/deleteFile/mod.ts
@@ -0,0 +1,20 @@
+import { coreApp } from "../../../mod.ts";
+import { setTokens, setUser, grantAccess } from "@lib";
+import { deleteFileFn } from "./deleteFile.fn.ts";
+import { deleteFileValidator } from "./deleteFile.val.ts";
+
+export const deleteFileSetup = () =>
+  coreApp.acts.setAct({
+    schema: "file",
+    actName: "deleteFile",
+    validationRunType: "delete",
+    validator: deleteFileValidator(),
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager"],
+      }),
+    ],
+    fn: deleteFileFn,
+  });
