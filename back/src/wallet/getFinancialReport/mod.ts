ef695c330231916cd69e12f29dd3561b99564e5a
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:23:37 +0330

    Add financial report endpoint for wallet managers

---
+++ back/src/wallet/getFinancialReport/mod.ts
@@ -0,0 +1,20 @@
+import { coreApp } from "../../../mod.ts";
+import { getFinancialReportFn } from "./getFinancialReport.fn.ts";
+import { getFinancialReportValidator } from "./getFinancialReport.val.ts";
+import { grantAccess, setTokens, setUser } from "@lib";
+
+export const getFinancialReportSetup = () =>
+  coreApp.acts.setAct({
+    schema: "wallet",
+    actName: "getFinancialReport",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager"],
+        features: ["wallet:admin"],
+      }),
+    ],
+    validator: getFinancialReportValidator(),
+    fn: getFinancialReportFn,
+  });
