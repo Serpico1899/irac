f76b2951427f5f90a257ec0eaf228624e2c615ea
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:26:16 +0330

    Add validate file integrity endpoint for managers

---
+++ back/src/file/validateFileIntegrity/mod.ts
@@ -0,0 +1,20 @@
+import { coreApp } from "../../../mod.ts";
+import { setTokens, setUser, grantAccess } from "@lib";
+import { validateFileIntegrityFn } from "./validateFileIntegrity.fn.ts";
+import { validateFileIntegrityValidator } from "./validateFileIntegrity.val.ts";
+
+export const validateFileIntegritySetup = () =>
+  coreApp.acts.setAct({
+    schema: "file",
+    actName: "validateFileIntegrity",
+    validationRunType: "get",
+    validator: validateFileIntegrityValidator(),
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager"],
+      }),
+    ],
+    fn: validateFileIntegrityFn,
+  });
