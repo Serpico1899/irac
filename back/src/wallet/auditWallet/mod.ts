f7c58f05d0c2c7ef5ff7b7320ce51a7af856f4e5
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:24:40 +0330

    Add wallet audit action setup

---
+++ back/src/wallet/auditWallet/mod.ts
@@ -0,0 +1,20 @@
+import { coreApp } from "../../../mod.ts";
+import { auditWalletFn } from "./auditWallet.fn.ts";
+import { auditWalletValidator } from "./auditWallet.val.ts";
+import { grantAccess, setTokens, setUser } from "@lib";
+
+export const auditWalletSetup = () =>
+  coreApp.acts.setAct({
+    schema: "wallet",
+    actName: "auditWallet",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager"],
+        features: ["wallet:admin"],
+      }),
+    ],
+    validator: auditWalletValidator(),
+    fn: auditWalletFn,
+  });
