564a662a84f67f9d602b4555ef39e47b60dd0803
serpico1899 <billywilder22@gmail.com>
2025-09-19 16:39:43 +0330

    Add getEnrollment function to fetch enrollment by ID

---
+++ back/src/enrollment/getEnrollment/getEnrollment.fn.ts
@@ -0,0 +1,13 @@
+import { type ActFn, ObjectId } from "@deps";
+import { enrollment } from "../../../mod.ts";
+
+export const getEnrollmentFn: ActFn = async (body) => {
+  const { set: { _id }, get } = body.details;
+
+  const foundEnrollment = await enrollment.findOne({
+    filter: { _id: new ObjectId(_id) },
+    projection: get,
+  });
+
+  return foundEnrollment;
+};
