2f58b1065bde433b76822ca7befb2af70461ae36
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:23:00 +0330

    Add handleDispute action to wallet schema

---
+++ back/src/wallet/handleDispute/mod.ts
@@ -0,0 +1,20 @@
+import { coreApp } from "../../../mod.ts";
+import { handleDisputeFn } from "./handleDispute.fn.ts";
+import { handleDisputeValidator } from "./handleDispute.val.ts";
+import { grantAccess, setTokens, setUser } from "@lib";
+
+export const handleDisputeSetup = () =>
+  coreApp.acts.setAct({
+    schema: "wallet",
+    actName: "handleDispute",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager"],
+        features: ["wallet:admin"],
+      }),
+    ],
+    validator: handleDisputeValidator(),
+    fn: handleDisputeFn,
+  });
