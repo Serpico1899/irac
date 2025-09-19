c6edfa9f809dcec61d38053fc584ab2e2a732e09
serpico1899 <billywilder22@gmail.com>
2025-09-19 23:29:08 +0330

    Add bulk file deletion functionality

---
+++ back/src/file/bulkDelete/mod.ts
@@ -0,0 +1,20 @@
+import { coreApp } from "../../../mod.ts";
+import { setTokens, setUser, grantAccess } from "@lib";
+import { bulkDeleteFn } from "./bulkDelete.fn.ts";
+import { bulkDeleteValidator } from "./bulkDelete.val.ts";
+
+export const bulkDeleteSetup = () =>
+  coreApp.acts.setAct({
+    schema: "file",
+    actName: "bulkDelete",
+    validationRunType: "delete",
+    validator: bulkDeleteValidator(),
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["Manager"],
+      }),
+    ],
+    fn: bulkDeleteFn,
+  });
