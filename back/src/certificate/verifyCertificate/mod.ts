45a555ac603121f7f71603a2a1144f0f8ba76319
serpico1899 <billywilder22@gmail.com>
2025-09-21 14:04:36 +0330

    Add certificate verification endpoint

---
+++ back/src/certificate/verifyCertificate/mod.ts
@@ -0,0 +1,16 @@
+import { coreApp } from "../../../mod.ts";
+import { verifyCertificateFn } from "./verifyCertificate.fn.ts";
+import { verifyCertificateValidator } from "./verifyCertificate.val.ts";
+
+export const verifyCertificateSetup = () =>
+  coreApp.acts.setAct({
+    schema: "enrollment",
+    actName: "verifyCertificate",
+    validationRunType: "get",
+    preAct: [],
+    validator: verifyCertificateValidator(),
+    fn: verifyCertificateFn,
+  });
+
+export const verifyCertificate = "verifyCertificate";
+export { verifyCertificateValidator };
