9a21aba329931a6d20288132ed46a855bb266ec6
serpico1899 <billywilder22@gmail.com>
2025-09-21 13:57:41 +0330

    Add getRecommendations endpoint to search module

---
+++ back/src/search/getRecommendations/mod.ts
@@ -0,0 +1,11 @@
+import { coreApp } from "../../mod.ts";
+import getRecommendationsHandler from "./getRecommendations.fn.ts";
+import { getRecommendationsValidator } from "./getRecommendations.val.ts";
+
+export const getRecommendationsSetup = () =>
+  coreApp.acts.setAct({
+    schema: "search",
+    actName: "getRecommendations",
+    validator: getRecommendationsValidator,
+    fn: getRecommendationsHandler,
+  });
