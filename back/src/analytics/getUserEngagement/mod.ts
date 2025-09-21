48e188b01ea3de761f5951cfc258e0823d9d0cc6
serpico1899 <billywilder22@gmail.com>
2025-09-21 14:07:15 +0330

    Add user engagement analytics setup module

---
+++ back/src/analytics/getUserEngagement/mod.ts
@@ -0,0 +1,13 @@
+import { setAct } from "../../../mod.ts";
+import { getUserEngagementValidator } from "./getUserEngagement.val.ts";
+import { getUserEngagementFn } from "./getUserEngagement.fn.ts";
+
+export const getUserEngagementSetup = () => {
+  setAct({
+    schema: {
+      details: getUserEngagementValidator.schema.details,
+    },
+    validator: getUserEngagementValidator,
+    fn: getUserEngagementFn,
+  });
+};
