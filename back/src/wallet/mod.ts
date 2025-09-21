900730c5218c10b7ee9c4896cad2fb80ec72eb35
serpico1899 <billywilder22@gmail.com>
2025-09-21 14:12:28 +0330

    Add admin wallet management operations

--- back/src/wallet/mod.ts
+++ back/src/wallet/mod.ts
@@ -3,6 +3,16 @@ import { getBalanceSetup } from "./getBalance/mod.ts";
 import { depositSetup } from "./deposit/mod.ts";
 import { purchaseSetup } from "./purchase/mod.ts";
 import { getTransactionsSetup } from "./getTransactions/mod.ts";
+import { adminDepositSetup } from "./adminDeposit/mod.ts";
+import { adminWithdrawSetup } from "./adminWithdraw/mod.ts";
+import { adjustBalanceSetup } from "./adjustBalance/mod.ts";
+import { freezeWalletSetup } from "./freezeWallet/mod.ts";
+import { unfreezeWalletSetup } from "./unfreezeWallet/mod.ts";
+import { getWalletStatsSetup } from "./getWalletStats/mod.ts";
+import { getAllWalletsSetup } from "./getAllWallets/mod.ts";
+import { getFinancialReportSetup } from "./getFinancialReport/mod.ts";
+import { auditWalletSetup } from "./auditWallet/mod.ts";
+import { handleDisputeSetup } from "./handleDispute/mod.ts";
 
 export const walletSetup = () => {
   getWalletSetup();
@@ -10,6 +20,16 @@ export const walletSetup = () => {
   depositSetup();
   purchaseSetup();
   getTransactionsSetup();
+  adminDepositSetup();
+  adminWithdrawSetup();
+  adjustBalanceSetup();
+  freezeWalletSetup();
+  unfreezeWalletSetup();
+  getWalletStatsSetup();
+  getAllWalletsSetup();
+  getFinancialReportSetup();
+  auditWalletSetup();
+  handleDisputeSetup();
 };
 
 export * from "./service.ts";
