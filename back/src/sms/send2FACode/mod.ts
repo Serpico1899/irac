9b0c27fbd252a92aec8a21964207f41963ed4982
serpico1899 <billywilder22@gmail.com>
2025-09-21 13:56:08 +0330

    Add send2FACode SMS action setup

---
+++ back/src/sms/send2FACode/mod.ts
@@ -0,0 +1,11 @@
+import { coreApp } from "../../../mod.ts";
+import { send2FACodeFn } from "./act.ts";
+import { send2FACodeValidator } from "./send2FACode.val.ts";
+
+export const send2FACodeSetup = () =>
+  coreApp.acts.setAct({
+    schema: "sms",
+    actName: "send2FACode",
+    validator: send2FACodeValidator(),
+    fn: send2FACodeFn,
+  });
