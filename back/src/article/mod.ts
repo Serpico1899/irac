70acf29bb0e68598b5cd71d6dc687a48a0e0e3c8
serpico1899 <billywilder22@gmail.com>
2025-09-21 14:13:21 +0330

    Add article management API endpoints

--- back/src/article/mod.ts
+++ back/src/article/mod.ts
@@ -1,5 +1,19 @@
 import { getArticlesSetup } from "./getArticles/mod.ts";
+import { createArticleSetup } from "./createArticle/mod.ts";
+import { updateArticleSetup } from "./updateArticle/mod.ts";
+import { deleteArticleSetup } from "./deleteArticle/mod.ts";
+import { getArticleSetup } from "./getArticle/mod.ts";
+import { publishArticleSetup } from "./publishArticle/mod.ts";
+import { archiveArticleSetup } from "./archiveArticle/mod.ts";
+import { getArticleStatsSetup } from "./getArticleStats/mod.ts";
 
 export const articleSetup = () => {
   getArticlesSetup();
+  createArticleSetup();
+  updateArticleSetup();
+  deleteArticleSetup();
+  getArticleSetup();
+  publishArticleSetup();
+  archiveArticleSetup();
+  getArticleStatsSetup();
 };
