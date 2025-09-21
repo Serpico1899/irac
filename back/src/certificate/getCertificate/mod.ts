8f2ed78d498df7f7d89e4223246c4beac0d88140
serpico1899 <billywilder22@gmail.com>
2025-09-21 14:06:02 +0330

    Add certificate retrieval endpoint

---
+++ back/src/certificate/getCertificate/mod.ts
@@ -0,0 +1,24 @@
+import type { MyContext } from "@lib";
+import { grantAccess, setTokens, setUser } from "@lib";
+import { coreApp } from "../../../mod.ts";
+import { getCertificateFn } from "./getCertificate.fn.ts";
+import { getCertificateValidator } from "./getCertificate.val.ts";
+
+export const getCertificateSetup = () =>
+  coreApp.acts.setAct({
+    schema: "enrollment",
+    actName: "getCertificate",
+    validationRunType: "get",
+    preAct: [
+      setTokens,
+      setUser,
+      grantAccess({
+        levels: ["User", "Admin", "Manager", "Instructor"],
+      }),
+    ],
+    validator: getCertificateValidator(),
+    fn: getCertificateFn,
+  });
+
+export const getCertificate = "getCertificate";
+export { getCertificateValidator };
