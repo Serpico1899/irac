8f716ef5afdc69c0c32d99f61346d855b8b6e885
serpico1899 <billywilder22@gmail.com>
2025-09-21 14:07:58 +0330

    Add revenue dashboard API endpoint setup

---
+++ back/src/analytics/getRevenueDashboard/mod.ts
@@ -0,0 +1,13 @@
+import { setAct } from "../../../mod.ts";
+import { getRevenueDashboardValidator } from "./getRevenueDashboard.val.ts";
+import { getRevenueDashboardFn } from "./getRevenueDashboard.fn.ts";
+
+export const getRevenueDashboardSetup = () => {
+  setAct({
+    schema: {
+      details: getRevenueDashboardValidator.schema.details,
+    },
+    validator: getRevenueDashboardValidator,
+    fn: getRevenueDashboardFn,
+  });
+};
